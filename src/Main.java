2af62842b29426866349f0f5a36787badaf94c18
Saipriya <vsaipriya48@example.com>
2026-07-08 14:24:03 +0530

    Add Supplier class

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ public class Main {
         Bill bill1 = new Bill("B001", "Priya", "9876543210");
         bill1.addItem(saree, 2);
         bill1.printBill();
+        //discount feature
         bill1.applyDiscount(10);
         bill1.printBill();
+        //supplier class
+        Supplier supplier1 = new Supplier("SUP001", "Chennai Textiles", "9876500000", "chennai@textiles.com");
+        supplier1.addProduct("Red Silk Saree");
+        supplier1.addProduct("Blue Cotton Shirt");
+        supplier1.printSupplierDetails();
     }
 }
